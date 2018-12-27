507de1f98d089682699a1d802572a5f552aae69b
LEE <fta350@gmail.com>
2018-12-27 23:52:48 +0900

    book9 가상의 원리와 다중상속 소스코드 작성

---
+++ Study_Cpp/book9_MultiInheri3.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+	Base() { cout << "Base Constructor" << endl; }
+	void SimpleFunc() { cout << "BaseOne" << endl; }
+};
+
+class MiddleDerivedOne : virtual public Base
+{
+public:
+	MiddleDerivedOne() : Base()
+	{
+		cout << "MiddleDerivedOne Constructor" << endl;
+	}
+	void MiddleFuncOne()
+	{
+		SimpleFunc();
+		cout << "MiddleDerivedOne" << endl;
+	}
+};
+
+class MiddleDerivedTwo : virtual public Base
+{
+public:
+	MiddleDerivedTwo() : Base()
+	{
+		cout << "MiddleDerivedTwo Constructor" << endl;
+	}
+	void MiddleFunTwo()
+	{
+		SimpleFunc();
+		cout << "MiddleDerivedTwo" << endl;
+	}
+};
+
+class LastDerived : public MiddleDerivedOne, public MiddleDerivedTwo
+{
+public:
+	LastDerived() : MiddleDerivedOne(), MiddleDerivedTwo()
+	{
+		cout << "LastDerived Constructor" << endl;
+	}
+	void ComplexFunc()
+	{
+		MiddleFuncOne();
+		MiddleFunTwo();
+		SimpleFunc();
+	}
+};
+
+int main(void)
+{
+	cout << "按眉 积己 傈 ....." << endl;
+	LastDerived ldr;
+	cout << "按眉 积己 饶 ....." << endl;
+	ldr.ComplexFunc();
+
+
+	return 0;
+}
